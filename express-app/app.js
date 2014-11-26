d0b3fc867fd3fe7ff62058bee2e3e9d312a52b76
Richard Morrison <richard@rmorrison.net>
2014-11-26 21:25:30 +0000

    Start on scheduled jobs

--- express-app/app.js
+++ express-app/app.js
@@ -3,6 +3,7 @@ var bodyParser   = require('body-parser');
 var app          = express();
 var jf           = require('jsonfile');
 var net          = require('net');
+var schedule     = require('node-schedule');
 var sockfile     = '/var/nitelite/communicate.sock';
 var settingsfile = '/var/nitelite/settings.json';
 var faye         = require('faye');
@@ -10,6 +11,7 @@ var bayeux       = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 var fayeClient   = bayeux.getClient();
 var state        = '';
 var settings;
+var jobs = false;
 
 jf.readFile(settingsfile, function(err, obj) {
     if (err) {
@@ -23,7 +25,42 @@ jf.readFile(settingsfile, function(err, obj) {
     } else {
         settings = obj;
     }
+    setJobs();
 });
+
+function setJobs() {
+    if (jobs) {
+        jobs.nitetime.cancel();
+        jobs.litetime.cancel();
+        jobs.offtime.cancel();
+    }
+    jobs = {
+        nitetime: schedule.scheduleJob(getRecurrenceRule(settings.times.nitetime), function() {
+            setState('nite');
+        });
+        litetime: schedule.scheduleJob(getRecurrenceRule(settings.times.litetime), function() {
+            setState('lite');
+        });
+        offtime: schedule.scheduleJob(getRecurrenceRule(settings.times.offtime), function() {
+            setState('off');
+        });
+    };
+    console.log('jobs set', jobs);
+    console.log('next nite', jobs.nitetime.nextInvocation());
+}
+
+function getRecurrenceRule(time) {
+    var hour   = parseInt(time.split(":")[0]);
+    var minute = parseInt(time.split(":")[1]);
+    var rule = new schedule.RecurrenceRule();
+    rule.hour = hour;
+    rule.minute = minute;
+    return rule
+}
+
+function setState(newState) {
+    client.write(newState);
+}
 
 // Inter-process communications with the python process
 var client = net.connect( { path: sockfile });
@@ -52,7 +89,7 @@ app.use('/css',              express.static('css'));
 app.use('/js',               express.static('js'));
 app.use(bodyParser.urlencoded());
 app.post('/state', function(req, res) {
-    client.write(req.body.state);
+    setState(req.body.state);
     res.redirect('/');
 });
 app.get('/state', function(req, res) {
@@ -62,7 +99,10 @@ app.post('/times', function(req, res) {
     settings.times.nitetime = req.body.nitetime;
     settings.times.litetime = req.body.litetime;
     settings.times.offtime  = req.body.offtime;
+
     jf.writeFileSync(settingsfile, settings);
+    setJobs();
+
     fayeClient.publish('/times', settings.times);
     res.send(settings.times);
 });
